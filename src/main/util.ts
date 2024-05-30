b9090febc8c3badc782437d43a01c14fc9d6a797
Andreas George Nechita <ga.nechita@gmail.com>
2024-05-30 14:40:32 +0200

    moved the global hotkey code to another file for readability and flexibility

--- src/main/util.ts
+++ src/main/util.ts
@@ -1,6 +1,8 @@
 /* eslint import/prefer-default-export: off */
 import { URL } from 'url';
+import fs from 'fs';
 import path from 'path';
+import { app, BrowserWindow, shell, ipcMain, globalShortcut } from 'electron';
 
 export function resolveHtmlPath(htmlFileName: string) {
   if (process.env.NODE_ENV === 'development') {
@@ -10,4 +12,32 @@ export function resolveHtmlPath(htmlFileName: string) {
     return url.href;
   }
   return `file://${path.resolve(__dirname, '../renderer/', htmlFileName)}`;
+}
+
+export function hotkey1() {
+  globalShortcut.register('Alt+O', () => {
+    console.log('-----------ELECTRON LOVES GLOBAL SHORTCUTS!---------------');
+    const snippetsDir = path.join(__dirname, 'snippets');
+
+    const loadSnippets = () => {
+      const snippets: Snippet[] = [];
+
+      try {
+        const files = fs.readdirSync(snippetsDir);
+
+        files.forEach((file) => {
+          const filePath = path.join(snippetsDir, file);
+          const content = fs.readFileSync(filePath, 'utf-8');
+          snippets.push({ title: file, content });
+        });
+      } catch (err) {
+        console.error('Error reading snippets directory:', err);
+      }
+
+      return snippets;
+    };
+
+    const snippets = loadSnippets();
+    console.log(snippets);
+  });
 }
